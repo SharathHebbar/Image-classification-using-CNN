76178f5eecaeee0a18c35386279af31e50f59079
SharathHebbar <Sharathhebbar24@gmail.com>
2022-01-16 21:34:07 +0530

    updates

---
+++ imageclassify.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+classes = {
+    0: 'aeroplane',
+    1: 'automobile',
+    2: 'bird',
+    3: 'cat',
+    4: 'deer',
+    5: 'dog',
+    6: 'frog',
+    7: 'horse',
+    8: 'ship',
+    9: 'truck'
+}
+
+
+@app.route('/', methods=['GET', 'POST'])
